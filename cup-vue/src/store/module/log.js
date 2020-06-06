7f085c98077103fdc4734a0f6701fc78e4e2e0a1
wuyanshen <763010522@qq.com>
2020-06-06 18:37:21 +0800

    新增日志显示

---
+++ cup-vue/src/store/module/log.js
@@ -0,0 +1,34 @@
+import api from '@/api/api'
+
+const state = {
+	
+}
+
+const actions = {
+	//日志分页查询
+	logPage({commit}, params){
+		return new Promise((resolve,reject) => {
+			api.log.logPage(params).then(res => {
+				resolve(res)
+			}).catch(error => {
+				reject(error)
+			})
+		})
+	},
+}
+
+const getters = {
+	
+}
+
+const mutations = {
+	
+}
+
+export default{
+	namespaced: true,
+	state,
+	actions,
+	getters,
+	mutations
+}
