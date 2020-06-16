be2e65eee439c1db9322536103b3c87b8a87397d
wuyanshen <763010522@qq.com>
2020-06-16 16:56:47 +0800

    更新发布配置

--- cup/deploy.sh
+++ cup/deploy.sh
@@ -1,64 +1,64 @@
 #!/bin/bash
 
-echo '获取当前工作分支:'
-curr_branch=`git symbolic-ref --short -q HEAD`
-echo '当前工作分支 => '${curr_branch}'\n'
-
-echo '拉取最新代码'
+printf '获取当前工作分支:\n'
+curr_branch=$(git symbolic-ref --short -q HEAD)
+printf "当前工作分支 => %s\n" "${curr_branch}"
+printf '拉取最新代码\n'
 git pull
-echo '\n'
+printf '\n'
 
-echo '打包'
+printf '打包\n'
 mvn clean package -Dmaven.test.skip=true
-echo '\n'
+printf '\n'
 
 
-echo '获取环境名'
+printf '获取环境名\n'
 env=''
 
-if [ x$1 != x ];then
+if [ x"$1" != x ];then
     env=$1
-    echo 'sh deploy.sh的运行参数不为空，参数的值是：'${env}
+    printf 'sh deploy.sh的运行参数不为空，参数的值是：%s\n'"${env}"
 else
     env='prod'
-    echo 'sh deploy.sh的运行参数为空'
+    printf 'sh deploy.sh的运行参数为空\n'
 fi
-echo '\n'
-
-echo '读取配置文件'
-deploy_host=`sed '/^'${env}_host'=/!d;s/.*=//' deploy.conf`
-deploy_user=`sed '/^'${env}_user'=/!d;s/.*=//' deploy.conf`
-deploy_path=`sed '/^'${env}_path'=/!d;s/.*=//' deploy.conf`
-deploy_project_name=`sed '/^'${env}_name'=/!d;s/.*=//' deploy.conf`
+printf '\n'
 
 
-echo '将jar和Dockerfile上传到服务器'
-ssh ${deploy_user}@${deploy_host} "cd /home; rm -rf cup-project; mkdir cup-project; cd cup-project; mkdir target"
-scp Dockerfile ${deploy_user}@${deploy_host}:${deploy_path}
-cd ./target
-scp ${deploy_project_name} ${deploy_user}@${deploy_host}:${deploy_path}/target
-echo '\n'
+printf '读取配置文件\n'
+deploy_host=$(sed '/^'${env}_host'=/!d;s/.*=//' deploy.conf)
+deploy_user=$(sed '/^'${env}_user'=/!d;s/.*=//' deploy.conf)
+deploy_path=$(sed '/^'${env}_path'=/!d;s/.*=//' deploy.conf)
+deploy_project_name=$(sed '/^'${env}_name'=/!d;s/.*=//' deploy.conf)
 
-ssh ${deploy_user}@${deploy_host} "
 
-echo '删除旧的docker镜像';
+printf '将jar和Dockerfile上传到服务器\n'
+ssh "${deploy_user}"@"${deploy_host}" "cd /home; rm -rf cup-project; mkdir cup-project; cd cup-project; mkdir target"
+scp Dockerfile "${deploy_user}"@"${deploy_host}":"${deploy_path}"
+cd ./target || exit
+scp "${deploy_project_name}" "${deploy_user}"@"${deploy_host}":"${deploy_path}"/target
+printf '\n'
+
+ssh "${deploy_user}"@"${deploy_host}" "
+
+printf '删除旧的docker镜像\n';
 docker rm -f cup
 docker rmi cup:1.0;
-echo '\n';
+printf '\n';
 
-echo '构建docker镜像';
+printf '构建docker镜像\n';
 cd ${deploy_path};
 docker ps;
 docker build -t cup:1.0 .;
 docker images ;
-echo '\n';
+printf '\n';
 
-echo '启动docker镜像';
+printf '启动docker镜像\n';
 docker run -d -p 3001:3001 --name cup cup:1.0
 docker ps;
-echo '\n';
+printf '\n';
 
-echo '部署成功'
+printf '部署成功\n'
 "
 
 
