d9388f98aa5c43dbc97060a707959ec7a8ffece3
wuyanshen <763010522@qq.ocm>
2021-08-23 12:32:14 +0800

    修改

--- cup/src/main/java/com/lvcoding/CupApp.java
+++ cup/src/main/java/com/lvcoding/CupApp.java
@@ -28,11 +28,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author wuyanshen
  * @date 2020-03-19 1:00 上午
  * @description 启动类
- *
- * 整合activiti会报错 GlobalAuthenticationConfigurerAdapter  cannot be opened because it does not exist
- * 这个是在 spring-boot-starter-security 依赖中的 属于安全配置类,
- * 而引入的activiti-spring-boot-starter-basic 依赖中也存在了一个自动安全配置类,  两个安全配置。
- * 排除activiti-spring-boot-starter-basic安全配置类
  */
 @SpringBootApplication
 public class CupApp {
